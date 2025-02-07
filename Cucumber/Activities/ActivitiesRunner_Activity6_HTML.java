ca6d5fbbbd042c8e264fd17fda7deb7ed092da97
nharshith <n.harshith@ibm.com>
2025-02-07 23:32:33 +0530

    Update ActivitiesRunner_Activity6_HTML.java

--- Cucumber/Activities/ActivitiesRunner_Activity6_HTML.java
+++ Cucumber/Activities/ActivitiesRunner_Activity6_HTML.java
@@ -1,18 +1,19 @@
 package runners;
 
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.platform.engine.Constants;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "features",
-		glue = {"stepDefinitions"},
-		tags = "@activity5",
-		plugin = {"html:target/cucumber-reports/index.html"},
-	    monochrome = true
-	)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity2")
+@ConfigurationParameter(
+	key = Constants.PLUGIN_PROPERTY_NAME,
+	value = "pretty, html:Reports/HTML_Report.html, junit:Reports/XML_Report.xml, json:Reports/JSON_Report.json")
 public class ActivitiesRunner_Activity6_HTML {
-
 }
